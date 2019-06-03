18151f6a841806d664e9ffae14d22c3e283e8dfa
takahashi-nobuhiro <takahashi.nobuhiro@dena.jp>
2019-06-03 01:46:31 -0700

    Initial Commit

---
+++ MojaveSampleApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MojaveSampleApp
+//
+//  Created by Nobuhiro Takahashi on 2019/06/03.
+//  Copyright © 2019 Nobuhiro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MojaveSampleApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MojaveSampleApp
+//
+//  Created by Nobuhiro Takahashi on 2019/06/03.
+//  Copyright © 2019 Nobuhiro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
